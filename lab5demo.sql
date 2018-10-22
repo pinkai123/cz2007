099a8e33e77e06e1c7489810471d96d7f5afb2b7
pinkai123 <kaiz123129@gmail.com>
2018-10-22 19:16:26 +0800

    Update lab5demo.sql

--- lab5demo.sql
+++ lab5demo.sql
@@ -1,3 +1,17 @@
+CREATE TABLE Orders(
+UsersID int,
+OrdersID int,
+EndDate datetime
+PRIMARY KEY(OrdersID)
+FOREIGN KEY UsersID REFERENCES Users(UsersID)
+ON DELETE CASCADE
+ON UPDATE CASCADE
+);
 
+CREATE TABLE CommentsOnComments(
+CommenterID int,
+ComenteeID int
+PRIMARY KEY(CommenterID)
+);
 
 
